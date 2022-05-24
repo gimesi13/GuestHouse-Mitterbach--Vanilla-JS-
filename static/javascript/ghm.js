6d5b488067212bda8acd910c7af7c74adb48e933
gimesi13 <105725861+gimesi13@users.noreply.github.com>
2022-05-24 11:41:02 +0200

    version 2

--- static/javascript/ghm.js
+++ static/javascript/ghm.js
@@ -37,20 +37,86 @@ if(e.target.className!=='nav-ul-active' && e.target.className!=='hamburgerimg'){
     hamburger.className = "nav-ul";
     hamburgermenu.innerHTML ='';
     hamburgermenu.appendChild(hamburgerimage);
-    console.log('now');
 }
 }
 
 
 //navbar scroll disappear
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+
+
+
+// background darkening on scrool
+window.onscroll = function() {myFunction()};
+
+let prevScrollpos = window.pageYOffset;
+function myFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 350) {
+    let wrapper = document.querySelector('#wrapper1');
+    wrapper.className = 'wrapper-1-dark';
+  } else {
+    let wrapper = document.querySelector('#wrapper1');
+    wrapper.className = 'wrapper-1';
+  }
+
+  let currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-1.5em";
   }
   prevScrollpos = currentScrollPos;
+}
+
+// rooms slider
+
+const roomsrightSlide = document.querySelector('#roomsslideright');
+const roomsleftSlide = document.querySelector('#roomsslideleft');
+
+roomsrightSlide.addEventListener('click', roomsSlide);
+roomsleftSlide.addEventListener('click', roomsSlide);
+
+function roomsSlide(e){
+  const radio1 = document.querySelector('#radio1');
+  const radio2 = document.querySelector('#radio2');
+  const radio3 = document.querySelector('#radio3');
+  const radio4 = document.querySelector('#radio4');
+  const radio5 = document.querySelector('#radio5');
+  const radio6 = document.querySelector('#radio6');
+  const radio7 = document.querySelector('#radio7');
+  const radio8 = document.querySelector('#radio8');
+
+  if(e.target.id === 'roomsslideright'){
+    if(radio1.checked === true){
+      radio2.checked = true;
+    } else if(radio2.checked === true){
+      radio3.checked = true;
+    } else if(radio3.checked === true){
+      radio4.checked = true;
+    } else if(radio4.checked === true){
+      radio5.checked = true;
+    } else if(radio5.checked === true){
+      radio6.checked = true;
+    } else if(radio6.checked === true){
+      radio7.checked = true;
+    } else if(radio7.checked === true){
+      radio8.checked = true;
+    }
+  } else if(e.target.id === 'roomsslideleft'){
+    if(radio8.checked === true){
+      radio7.checked = true;
+    } else if(radio2.checked === true){
+      radio1.checked = true;
+    } else if(radio3.checked === true){
+      radio2.checked = true;
+    } else if(radio4.checked === true){
+      radio3.checked = true;
+    } else if(radio5.checked === true){
+      radio4.checked = true;
+    } else if(radio6.checked === true){
+      radio5.checked = true;
+    } else if(radio7.checked === true){
+      radio6.checked = true;
+    }
+  }
 }
